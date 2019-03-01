a96d9d5d67c5080d4cd14edf6ddb11f4df4e1c30
Daniel She <shekwancheung0528@gmail.com>
2019-03-01 16:22:11 +0800

    Random Game

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1551428467,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/plane-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/turret-sheet0.png",
+		"images/lasere-sheet0.png",
+		"images/healthbar-sheet0.png",
+		"images/healthbarbg-sheet0.png",
+		"images/startbutton-sheet0.png",
+		"media/laser.m4a",
+		"media/laser.ogg",
+		"media/explosion.m4a",
+		"media/explosion.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
